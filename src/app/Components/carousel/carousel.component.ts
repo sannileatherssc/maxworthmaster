292ca80266a1282fc89474504db9539618a5cce8
sanni@jobulary.io <sanni@leatherssc.org>
2022-07-13 15:23:54 +0530

    v2.0 carousel

--- src/app/Components/carousel/carousel.component.ts
+++ src/app/Components/carousel/carousel.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { map, Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent implements OnInit {
+  temp: number = 3;
+  timerSubscription: Subscription | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    // this.showSlides(this.slideIndex);
+    this.timerSubscription = timer(0, 5000)
+      .pipe(
+        map(() => {
+          this.plus(); // load data contains the http request
+        })
+      )
+      .subscribe();
   }
 
+  plus() {
+    if (this.temp < 3) {
+      this.temp++;
+    } else {
+      this.temp = 0;
+    }
+  }
 }
+
+
+
+
